3742586d5465d5626c91a751db18eea4f9f00800
Sujin Pongchababnapa <127624492+Jintpong@users.noreply.github.com>
2024-12-05 15:20:25 +0000

    create bot move

---
+++ Bot.java
@@ -0,0 +1,41 @@
+import java.lang.Math;
+
+public class Bot {
+    private int bot_position_X;
+    private int bot_position_Y;
+    private int botGold;
+
+    public Bot(){
+        this.bot_position_X = bot_position_X;
+        this.bot_position_Y = bot_position_Y;
+        this.botGold = 0;
+    }
+    //The idea behind the bot finding the position of the player is to use formula for the Eucilidean Distance 
+    public String BotCommand(char[][] map, int playerX, int playerY){
+        String[] moves = {"MOVE N", "MOVE S", "MOVE W", "MOVE E"}
+        // Direction of change e.g. (1,0) = S or (0,1) = E
+        int change_in_x = {-1, 1, 0, 0};
+        int change_in_y = {0 ,0, 1, -1};
+        //distance taks the maximum value a double can represent
+        double min_distance = DOUBLE.MAX_VALUE;
+        String bestmove = "";
+
+        // Checking for each moves
+        for (int i = 0; i < moves.length; i++){
+            int newbotX = botX + change_in_x[i];
+            int newbotY = botY + change_in_y[i];
+        } 
+        //Checking that its not a wall
+        if (newbotX >= 0 && newbotX < map.length && newbotY >= 0 && newbotY < map[0].length && map[b][newbotY] != '#') {
+            // Calculate the distance to the player
+            double distance_to_player = Math.sqrt(Math.pow(playerX - newbotX, 2) + Math.pow(playerY - newbotY, 2));
+            
+            //Updating the shortest move to the player
+            if (distance_to_player < min_distance){
+                min_distance = distance_to_player;
+                bestmove = moves[i];
+                }
+
+            }
+
+}

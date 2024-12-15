2732b99614cbc60af6bd8775747d9ce2da09b3b8
Sujin Pongchababnapa <127624492+Jintpong@users.noreply.github.com>
2024-12-15 13:51:58 +0000

    player can choose map

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Create an instance of the Map class
-        Map game_map = new Map();
+        Scanner scanner = new Scanner(System.in);
+        Map gameMap = new Map();
 
-        // Get the map
-        char[][] map = game_map.get_Map();
+        // Ask the user to select the map
+        System.out.println("Please select a map to play: ");
+        System.out.println("Choose 1 for Map 1 ");
+        System.out.println("Choose 2 for Map 2 ");
+        System.out.println("Choose 3 for Map 3 ");
+        System.out.println("Enter your choic:  ");
 
+        // check the user input 
+        int choice = scanner.nextInt();
 
-        Command command = new Command(map);
-
-        for (char[] i : map) {
-            System.out.println(new String(i));
-        }
+        //Get the map base on the chosen choice
+        char[][] chosenMap = gameMap.get_Map(choice);
         
+        
+        //display map
+        for (char[] row : chosenMap){
+            System.out.println(new String(row));
+        }
+
+        Command command = new Command(chosenMap);
         command.UserInput();
 
     }
